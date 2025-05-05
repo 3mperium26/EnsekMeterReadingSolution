a62689b66948c2aa1236f3ce2cc6bf555ab65f36
3mperium <3mperium@DESKTOP-6OHGVM9>
2025-05-06 00:50:54 +0100

    Update web client layout and fix upload action error

--- Ensek.MeterReadings.Web/Program.cs
+++ Ensek.MeterReadings.Web/Program.cs
@@ -149,7 +149,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     // Use standard MVC error handler for production
-    app.UseExceptionHandler("/Shared/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see [https://aka.ms/aspnetcore-hsts](https://aka.ms/aspnetcore-hsts).
     app.UseHsts();
 }
